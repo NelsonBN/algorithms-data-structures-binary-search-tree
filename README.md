da64f167cbb9db5a41b31c9379ea06464b05ce87
Nelson Nobre <technobre@gmail.com>
2024-06-15 00:06:55 +0100

    Added an example `Create from Sorted Array`

--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 - [Search](./src/1.search.py)
 - [Delete](./src/2.delete.py)
 - [Traversal](./src/3.traversal.py)
-- [Balancing using Sorting](./src/4.balancing_using_sorting.py)
+- [Create from Sorted Array](./src/4.create_from_sorted_array.py)
+- [Balancing using Sorting](./src/5.balancing_using_sorting.py)
 
 
 ## Demonstration
---
+++ src/4.create_from_sorted_array.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self, arr):
+        arr.sort()
+        self.__root = self.__balance(arr, 0, len(arr) - 1)
+
+
+    def __balance(self, nodes, start, end):
+        if start > end:
+            return None
+        mid = start + (end - start) // 2
+        node = Node(nodes[mid])
+        node.left = self.__balance(nodes, start, mid - 1)
+        node.right = self.__balance(nodes, mid + 1, end)
+        return node
+
+
+    def print_tree(self):
+        self.__print_tree(self.__root, "", True)
+
+    def __print_tree(self, node, indent, last):
+        if node is not None:
+            print(indent, end='')
+            if last:
+                print("R----", end='')
+                indent += "     "
+            else:
+                print("L----", end='')
+                indent += "|    "
+
+            print(node.key)
+            self.__print_tree(node.left, indent, False)
+            self.__print_tree(node.right, indent, True)
+
+
+values = [10, 5, 15, 2, 7, 12, 20]
+
+bst = BinarySearchTree(values)
+bst.print_tree()
